ddffdc15da50e7d5f1f39d0f73a71dbbe5152686
RaresColocosel <rarescolocosel@yahoo.com>
2025-03-24 08:00:08 +0200

    config(Backend): Configure rquests in controllers
    The controllers use a GET request with a path variable to initialize the index

--- LocalSearchEngine/src/main/java/com/example/LocalSearchEngine/Backend/IndexController.java
+++ LocalSearchEngine/src/main/java/com/example/LocalSearchEngine/Backend/IndexController.java
@@ -1,4 +1,27 @@
 package com.example.LocalSearchEngine.Backend;
 
+import com.example.LocalSearchEngine.Backend.ServiceClass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/indexer")
 public class IndexController {
+
+    private final ServiceClass serviceClass;
+
+    @Autowired
+    public IndexController(ServiceClass serviceClass) {
+        this.serviceClass = serviceClass;
+    }
+
+    // Using a GET request with a path variable "/indexer/initialize/{directory}"
+    // e.g. GET http://localhost:8080/indexer/initialize/C:/myfiles
+    @GetMapping("/initialize/{directory}")
+    public ResponseEntity<String> reindex(@PathVariable String directory) {
+        serviceClass.indexDirectory(directory);
+        String msg = "Incremental indexing started for directory: " + directory;
+        return ResponseEntity.ok(msg);
+    }
 }
--- LocalSearchEngine/src/main/java/com/example/LocalSearchEngine/Backend/MainController.java
+++ LocalSearchEngine/src/main/java/com/example/LocalSearchEngine/Backend/MainController.java
@@ -1,4 +1,13 @@
 package com.example.LocalSearchEngine.Backend;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Local Search Engine!";
+    }
 }
